59a94cb938bc1233e29c7c64953c4027e472b5ad
Praise <oluwadhammieh@gmail.com>
2025-05-26 16:51:06 +0100

    feat(updates): Ad clan - Ileyah - statisense to projects

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,35 +1,51 @@
 let projects = [
   {
-    id: 'clan',
-    category: 'web',
-    title: 'Clan africa',
-    image: '/images/projects/clan.png',
-    link: 'https://creditclan.com',
-    description: `Clan helps businesses & individuals offer point of need credit to consumers across Africa. Built with VueJS and Bootstrap`
+    id: "ileyah",
+    category: "web",
+    title: "ILEYAH",
+    image: "/images/projects/ileyah.png",
+    link: "https://ileyah.com/",
+    description: `Statisense is helping millions of tenants pay their rent in easy, monthly subscriptions across Africa.`,
   },
   {
-    id: 'cribstock',
-    category: 'web',
-    title: 'Cribstock',
-    image: '/images/projects/cribstock.png',
-    link: 'https://www.cribstock.com',
-    description: `Cribstock is a community where young professionals can participate to own real estate as a means to wealth creation. Built with React, Next.js & Chakra UI`
+    id: "statisense",
+    category: "web",
+    title: "Statisense",
+    image: "/images/projects/statisense.png",
+    link: "https://statisense.co/",
+    description: `Statisense help businesses aggregate and convert complex customer data into simple visual insights using AI`,
   },
   {
-    id: 'mizala-p2p',
-    category: 'web',
-    title: 'Mizala p2p insurance',
-    image: '/images/projects/mizala-p2p.png',
-    link: 'https://p2p.mizala.co',
-    description: `Mizala p2p is a peer-to-peer insurance network of over 2,000 people, where individuals pool their premiums together to insure against a common risk. Built with React, NextJS & Tailwind`
+    id: "clan",
+    category: "web",
+    title: "Clan africa",
+    image: "/images/projects/clan.png",
+    link: "https://creditclan.com",
+    description: `Clan helps businesses & individuals offer point of need credit to consumers across Africa. Built with VueJS and Bootstrap`,
   },
   {
-    id: 'mizala',
-    category: 'web',
-    title: 'Mizala official website',
-    image: '/images/projects/mizala.png',
-    link: 'https://mizala.co',
-    description: `Mizala aims to make insurance available and affordable for Nigerians and offers a variety of products ranging from auto insurance to peer to peer insurance. Built with React, NextJS & Tailwind`
+    id: "cribstock",
+    category: "web",
+    title: "Cribstock",
+    image: "/images/projects/cribstock.png",
+    link: "https://www.cribstock.com",
+    description: `Cribstock is a community where young professionals can participate to own real estate as a means to wealth creation. Built with React, Next.js & Chakra UI`,
+  },
+  {
+    id: "mizala-p2p",
+    category: "web",
+    title: "Mizala p2p insurance",
+    image: "/images/projects/mizala-p2p.png",
+    link: "https://p2p.mizala.co",
+    description: `Mizala p2p is a peer-to-peer insurance network of over 2,000 people, where individuals pool their premiums together to insure against a common risk. Built with React, NextJS & Tailwind`,
+  },
+  {
+    id: "mizala",
+    category: "web",
+    title: "Mizala official website",
+    image: "/images/projects/mizala.png",
+    link: "https://mizala.co",
+    description: `Mizala aims to make insurance available and affordable for Nigerians and offers a variety of products ranging from auto insurance to peer to peer insurance. Built with React, NextJS & Tailwind`,
   },
   // {
   //   id: 'lcu',
@@ -62,14 +78,14 @@ let projects = [
   //   link: 'https://tasksmgr.herokuapp.com',
   //   github: 'https://github.com/jsbuddy/tasks'
   // },
-  {
-    id: 'fieldoracle',
-    category: 'web',
-    title: 'Fieldoracle',
-    image: '/images/projects/fieldoracle.png',
-    link: 'https://fieldoracle.com',
-    description: `Fieldoracle is an online marketplace connecting researchers with freelance agents to assist with data collection on the field. Built with Vue JS and Tailwind CSS`
-  },
+  // {
+  //   id: "fieldoracle",
+  //   category: "web",
+  //   title: "Fieldoracle",
+  //   image: "/images/projects/fieldoracle.png",
+  //   link: "https://fieldoracle.com",
+  //   description: `Fieldoracle is an online marketplace connecting researchers with freelance agents to assist with data collection on the field. Built with Vue JS and Tailwind CSS`,
+  // },
   // {
   //   id: 'taximoni',
   //   category: 'web',
@@ -78,14 +94,14 @@ let projects = [
   //   link: 'https://taximoni.com',
   //   description: `Taximoni offers short term loans to Taxi drivers within certain communities. Built with NuxtJS and Ant design`
   // },
-  {
-    id: 'sonoracredit',
-    category: 'web',
-    title: 'Sonora Credit',
-    image: '/images/projects/sonoracredit.png',
-    link: 'https://sonoracredit.com',
-    description: `Sonora credit provides simple and unique loans for all categories of people including businesses and individuals. Built with NextJS, Tailwind and Material css`
-  },
+  // {
+  //   id: "sonoracredit",
+  //   category: "web",
+  //   title: "Sonora Credit",
+  //   image: "/images/projects/sonoracredit.png",
+  //   link: "https://sonoracredit.com",
+  //   description: `Sonora credit provides simple and unique loans for all categories of people including businesses and individuals. Built with NextJS, Tailwind and Material css`,
+  // },
   // {
   //   id: 'vendor-advisor',
   //   category: 'web',
